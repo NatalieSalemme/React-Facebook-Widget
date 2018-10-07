b7c2d0c8e34ee9cf0a20fff8d01f6e0c94db3bcb
Natalie Salemme <NatalieSalemme@gmail.com>
2018-10-06 22:34:09 -0700

    changing hover color on three dots

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import ThreeDots from './ThreeDots';
 import Username from './Username';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons';
+
+
 import './App.css';
 
 class App extends Component {
@@ -9,6 +14,7 @@ class App extends Component {
       <div className="App">
         <Username />
         <ThreeDots />
+            <FontAwesomeIcon icon="far fa-bookmark" />
       </div>
     );
   }
--- src/ThreeDots.js
+++ src/ThreeDots.js
@@ -1,17 +1,27 @@
 import React from 'react';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+// library.add(faBookmark);
 
 const ThreeDots = props => {
+  const bookmark = <FontAwesomeIcon icon={faBookmark} />;
   return (
     <div>
       <div className="dropdown">
         <button href="#" className="dropbtn">...</button>
         <div className="dropdown-content">
-          <a href="#">Save post</a>
+          <a href="#" className="under-horizontal-bar">
+            <span className="bookmark">{bookmark}</span>
+              Save post
+            </a>
           <a href="#">Edit Post</a>
           <a href="#">Change Date</a>
           <a href="#">Turn off notifications for this post</a>
-          <a href="#">Show in Tab</a>
-          <a href="#">Hide from timeline</a>
+          <a href="#" c>Show in Tab</a>
+          <a href="#" className="under-horizontal-bar">Hide from timeline</a>
           <a href="#">Delete</a>
           <a href="#">Turn Off Translations</a>
         </div>
