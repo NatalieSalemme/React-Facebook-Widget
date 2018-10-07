62f31d817c5f583c7f21efd4e9adb41a2c8f6c50
Natalie Salemme <NatalieSalemme@gmail.com>
2018-10-07 11:10:29 -0700

    adding caret down to users

--- src/Username.js
+++ src/Username.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUsers} from '@fortawesome/free-solid-svg-icons';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 const Username = props => {
-  const usersIcon = <FontAwesomeIcon icon={faUsers} />
+  const usersIcon = <FontAwesomeIcon icon={faUsers} size="xs"/>
+  const caretDown = <FontAwesomeIcon icon={faCaretDown} size="xs"/>
   return (
     <div className="username-container">
       <div className="avatar-container">
@@ -12,8 +13,13 @@ const Username = props => {
     </div>
     <div className="username-column">
      <h6 className="username">{props.avatarName}</h6>
-     <p className="username-time">September 15 at 11:09 AM
-       <span className="users-icon">{usersIcon}</span></p>
+      <div className="time-row">
+      <p className="username-time">September 15 at 11:09 AM </p>
+       <div className="users-icon-dropdown">
+       <span className="users-icon">{usersIcon}</span>
+       <span className="caret-down">{caretDown}</span>
+     </div>
+   </div>
    </div>
   </div>
   );
