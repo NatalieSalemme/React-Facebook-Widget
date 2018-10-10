87396e3ce7b2beaf888b5ab49c6cdd94415e5aa4
Natalie Salemme <NatalieSalemme@gmail.com>
2018-10-10 14:23:19 -0700

    adding clickable dropdown menu to your friends icon

--- src/components/UserDropDown.js
+++ src/components/UserDropDown.js
@@ -10,37 +10,65 @@ export class UserDropDown extends React.Component {
       showMenu: false
     };
     this.showMenu = this.showMenu.bind(this);
-    this.closeMenu = this.showMenu.bind(this);
   }
-  showMenu(event) {
-    event.preventDefault();
-    console.log('clicked showed menu');
-    this.setState({ showMenu: true }, () => {
-      document.addEventListener('click', this.closeMenu);
+
+  showMenu = e => {
+    e.preventDefault();
+    this.setState({
+      showMenu: !this.state.showMenu
     });
-  }
-  closeMenu(event) {
-    if(!this.dropdownMenu.contains(event.target)) {
-      this.setState({ showMenu: false }, () => {
-        document.removeEventListener('click', this.closeMenu);
-      });
-    }
   }
   render() {
     const usersIcon = <FontAwesomeIcon icon={faUsers} size="xs"/>
     const caretDown = <FontAwesomeIcon icon={faCaretDown} size="xs"/>
     return (
       <div>
-        <div className="users-icon-dropdown tooltip" onClick={this.showMenu}>
-         <span className="tooltiptext">Your friends</span>
-       <span className="users-icon">{usersIcon}</span>
-       <span className="caret-down">{caretDown}</span>
-       </div>
+        <div onClick={this.showMenu}
+          className="users-icon-dropdown tooltip">
+           <span className="tooltiptext">Your friends</span>
+           <span className="users-icon">{usersIcon}</span>
+          <span className="caret-down">{caretDown}</span>
+        </div>
+        {
+          this.state.showMenu ? (
+            <div
+              className="menu"
+              ref={(element) => {
+                this.dropdownMenu = element;
+              }}
+              >
+            <div>
+                <button>Public</button>
+                <button>Friends</button>
+                <button>Friends except...</button>
+                <button>Only me</button>
+                <button>More...</button>
+              </div>
+            </div>
+          )
+          : (
+            null
+          )
+        }
     </div>
-
     );
   }
 }
+//   render() {
+//     const usersIcon = <FontAwesomeIcon icon={faUsers} size="xs"/>
+//     const caretDown = <FontAwesomeIcon icon={faCaretDown} size="xs"/>
+//     return (
+//       <div>
+//         <div className="users-icon-dropdown tooltip" onClick={this.showMenu}>
+//          <span className="tooltiptext">Your friends</span>
+//        <span className="users-icon">{usersIcon}</span>
+//        <span className="caret-down">{caretDown}</span>
+//        </div>
+//     </div>
+//
+//     );
+//   }
+// }
 export default UserDropDown;
 
 // export class UserDropDown extends React.Component {
