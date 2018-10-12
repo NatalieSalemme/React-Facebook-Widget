3ae7e9d432ec8e4f0c1777757500fa195e583bb5
Natalie Salemme <NatalieSalemme@gmail.com>
2018-10-12 12:01:20 -0700

    adding hover effect for avatar

--- src/components/Username.js
+++ src/components/Username.js
@@ -5,139 +5,69 @@ import PropTypes from 'prop-types';
 
 const Username = props => {
 
-  let todaysDate = new Date();
-  let militaryHours = todaysDate.getHours();
-  let hours = militaryHours > 12 ? militaryHours - 12 : militaryHours;
-  let amOrPM = militaryHours > 12 ? 'pm' : 'am';
-  let militaryMinutes = todaysDate.getMinutes();
-  let minutes = militaryMinutes < 10 ? '0' + militaryMinutes : militaryMinutes;
-  let shortDate = (todaysDate.getMonth() + 1) + '/' + todaysDate.getDate() + '/' +
-     todaysDate.getFullYear() + ' ' + hours + ':' + minutes + ' ' +
-     amOrPM;
 
-  return (
-    <div className="username-container">
-      <div className="avatar-container">
-      <span>{props.avatarImg}</span>
-    </div>
-    <div className="username-column">
-     <h6 className="username">{props.avatarName}</h6>
-      <div className="time-row">
-        <div className="time-tooltip">
-      <p className="username-time">September 15 at 11:09 AM </p>
-      <span className="time-tooltiptext">{shortDate.toString()}</span>
-    </div>
-      <UserDropDown />
-   </div>
-   </div>
-  </div>
-  );
+   let todaysDate = new Date();
+   let militaryHours = todaysDate.getHours();
+   let hours = militaryHours > 12 ? militaryHours - 12 : militaryHours;
+   let amOrPM = militaryHours > 12 ? 'pm' : 'am';
+   let militaryMinutes = todaysDate.getMinutes();
+   let minutes = militaryMinutes < 10 ? '0' + militaryMinutes : militaryMinutes;
+   let shortDate = (todaysDate.getMonth() + 1) + '/' + todaysDate.getDate() + '/' +
+      todaysDate.getFullYear() + ' ' + hours + ':' + minutes + ' ' +
+      amOrPM;
+      return (
+
+        <div className="username-container">
+          <div className="avatar-container avatar-hover">
+
+          <span className="avatar-img">{props.avatarImg}</span>
+          <span className="avatar-hover-text">
+
+            testing avatar image
+          </span>
+        </div>
+        <div className="username-column">
+         <h6 className="username">{props.avatarName}</h6>
+          <div className="time-row">
+            <div className="time-tooltip">
+          <p className="username-time">September 15 at 11:09 AM </p>
+          <span className="time-tooltiptext">{shortDate.toString()}</span>
+        </div>
+          <UserDropDown />
+       </div>
+       </div>
+      </div>
+      );
+
 }
 Username.propTypes = {
   avatarName: PropTypes.string.isRequired
 }
 export default Username;
 
+
 //
-// export class LikeCommentShare extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       showMenu: false
-//     }
-//     this.showMenu = this.showMenu.bind(this);
-//   }
-// showMenu = e => {
-//   e.preventDefault();
-//   this.setState({
-//     showMenu: !this.state.showMenu
-//   });
-// }
-//
-//
-//   render() {
-//   const thumbsUpIcon = <FontAwesomeIcon icon={faThumbsUp} size="lg"/>
-//   const commentIcon = <FontAwesomeIcon icon={faCommentAlt} size="lg"/>
-//   const shareIcon = <FontAwesomeIcon icon={faShare} size="lg"/>
-//   const heartIcon = <FontAwesomeIcon icon={faHeart} size="xs"/>
-//   const laughIcon = <FontAwesomeIcon icon={faLaughSquint} size="xs"/>
-//   const wowIcon = <FontAwesomeIcon icon={faSurprise} size="xs"/>
-//   const sadIcon = <FontAwesomeIcon icon={faSadTear} size="xs"/>
-//   const angryIcon = <FontAwesomeIcon icon={faAngry} size="xs"/>
-//   const smThumbsUpIcon = <FontAwesomeIcon icon={faThumbsUp} size="xs"/>
-//
-//
+// const ThreeDots = props => {
+//   const bookmark = <FontAwesomeIcon icon={faBookmark} />;
 //   return (
-//     <div>
-//     <div
-//       className="like-comment-share-bar" >
-//       <p className="likes-icon-container tooltip">
-//         <span style={{marginRight: '3px', color: '#3B5998'}}>{thumbsUpIcon}</span>
-//         <span
-//           onClick={this.props.addLike}
-//           >Like</span>
-//         <span className="tooltiptext emoji-hover" style={{marginLeft: '30px', height: '20px'}}>
-//              <span className="likes-icon" onClick={this.props.addLike}>{smThumbsUpIcon}</span>
-//              <span className="heart-icon" onClick={this.props.addLove}>{heartIcon}</span>
-//              <span className="laugh-icon">{laughIcon}</span>
-//              <span className="wow-icon">{wowIcon}</span>
-//              <span className="sad-icon">{sadIcon}</span>
-//              <span className="angry-icon">{angryIcon}</span>
-//           </span>
-//       </p>
-//       <p
-//         className="comments-icon-container"
-//         onClick={this.props.handleCommentInput}
-//         // onClick={props.focusOnInput}
-//         >
-//         <span style={{marginRight: '3px'}}>
-//           {commentIcon}
-//         </span>
-//         <span>Comment</span>
-//       </p>
-//       <p onClick={this.showMenu} className="tooltip share-icon-container">
-//         <span style={{marginRight: '3px', color: '#ed3b68'}}>
-//           {shareIcon}
-//         </span>Share</p>
-//       {
-//         this.state.showMenu ? (
-//           <div
-//             ref={(element) => {
-//               this.dropdownMenu = element;
-//             }}
-//             >
-//             <div className="triangle"></div>
-//             <div className="share-container">
-//
-//               <div className="share-dd-btn">
-//                 <span>Share Now (Friends)</span>
-//               </div>
-//               <div className="share-dd-btn">
-//                 <span>Share in a Group</span>
-//                 </div>
-//               <div className="share-dd-btn">
-//                 <span>Share...</span>
-//               </div>
-//               <div className="share-dd-btn">
-//                 <span>Send as Message</span>
-//               </div>
-//               <div className="share-dd-btn">
-//                 <span>Share on a friend's timeline</span>
-//               </div>
-//             </div>
-//           </div>
-//         )
-//         : (
-//           null
-//         )
-//       }
+//     <div className="dots-container">
+//       <div className="dropdown">
+//         <button href="https://www.facebook.com/" className="dropbtn">...</button>
+//         <div className="dropdown-content">
+//           <a href="https://www.facebook.com/" className="under-horizontal-bar">
+//             <span className="bookmark">{bookmark}</span>
+//               Save post
+//             </a>
+//           <a href="https://www.facebook.com/">Edit Post</a>
+//           <a href="https://www.facebook.com/">Change Date</a>
+//           <a href="https://www.facebook.com/">Turn off notifications for this post</a>
+//           <a href="https://www.facebook.com/">Show in Tab</a>
+//           <a href="https://www.facebook.com/" className="under-horizontal-bar">Hide from timeline</a>
+//           <a href="https://www.facebook.com/">Delete</a>
+//           <a href="https://www.facebook.com/">Turn Off Translations</a>
+//         </div>
+//       </div>
 //     </div>
-//   </div>
-//     );
-//     }
-//   }
-//   // LikeCommentShare.propTypes = {
-//   //   focusOnInput: PropTypes.func.isRequired
-//   // }
-//   export default LikeCommentShare;
-//
+//   )
+// }
+// export default ThreeDots;
